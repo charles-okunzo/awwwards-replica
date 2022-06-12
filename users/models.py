afff308b216ae4bbe8bfb3e05cdec27985aae4f8
Guyo <you@example.com>
2022-06-12 13:28:34 +0300

    feature --add profile image downsize capability

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 # Create your models here.
 
@@ -9,6 +11,18 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='profile', default = 'user7.jpg', blank= True)
     bio = models.TextField(blank= True)
     contacts = models.CharField(max_length=100, blank= True)
+
+
+    def save(self, *args, **kwargs):
+        super().save(self, *args, **kwargs)
+
+        img = Image.open(self.profile_pic.path)
+
+        if img.width > 300 or img.height >300:
+            output_size = (300, 300)
+
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
 
 
     def __str__(self) -> str:
