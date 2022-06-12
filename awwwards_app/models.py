b97af09b5bc2f106369073099601bcc6f1ef1467
Guyo <you@example.com>
2022-06-12 12:35:30 +0300

    feature --add Project model

--- awwwards_app/models.py
+++ awwwards_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, related_name='project', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'projects', blank = True, null = True)
+    description = models.TextField()
+    link = models.CharField(max_length=150)
